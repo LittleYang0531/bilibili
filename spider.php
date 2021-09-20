f0cbe764332f1e60700df23d24aa132ee500c220
优秀的小杨同学 <63852815+LittleYang0531@users.noreply.github.com>
2021-09-21 00:00:42 +0800

    Update spider.php

--- spider.php
+++ spider.php
@@ -12,14 +12,17 @@
 		curl_close($ch);
 		return $output;
 	}
-        $page=0;
+        $page=0;$urls=array();
         while (1){
 		
 	$array=json_decode(strip_tags(GetFromHTML("https://api.obfs.dev/api/pixiv/member_illust?id=".$argv[1]."&page=".(++$page))),true);
 	for ($i=0;$i<count($array["illusts"]);$i++) 
 	{
+		
+		
+		
 		$url=$array["illusts"][$i]["meta_single_page"]["original_image_url"];
-		$url=str_replace("i.pximg.net","proxy-jp1.pixivel.moe",$url);
+		$url=str_replace("i.pximg.net","proxy-jp1.pixivel.moe",$url);if ($urls[$url]) continue;else $urls[$url]=1;
 		shell_exec("wget ".$url);
 	}
 		if (count($array["illusts"])==0) break;
